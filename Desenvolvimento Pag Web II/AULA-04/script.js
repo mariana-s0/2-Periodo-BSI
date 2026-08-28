6a5268ea0ed7c45bf9c60c6880f32c13c171da77
Mariana <sousa06390@gmail.com>
2026-08-28 11:17:29 -0300

    Add files via upload

---
+++ Desenvolvimento Pag Web II/AULA-04/script.js
@@ -0,0 +1,32 @@
+const object_iphone_17 = {
+    "modelo" : "iphone_17",
+    "marca" : "apple",
+    "sistema operacional" : "IOS",
+    "suporte de redes" : "5G",
+    "resistente á agua" : "true",
+    "peso" : 177, 
+    "cores" : ["preto","branco","azul","rosa"],
+
+    'tela' : {
+        "tela" : "6,3_polegadas",
+        "tecnologia" : "OLED",
+        "taxa de atualização" : "120_Hz",
+        "recurso tela ligada" : "true"},
+
+        'camera' : {
+            "camera frontal" : "24_megapixels",
+            "camera traseira": ["câmera_principal_48_megapixels" , "câmera_ultrawide_48_megapixels"],
+            "resolução" : "4K"},
+
+    "armazanamento" : ["256_GB", "512_GB"],
+    'bateria' : {
+        "capacidade bateria" : "3692_mAh",
+        "suporte para carregamento sem fio" : "true"}
+};
+
+undefined
+console.log(object_iphone_17.cores);
+1 (4) ['preto', 'branco', 'azul', 'rosa']
+undefined
+
+
