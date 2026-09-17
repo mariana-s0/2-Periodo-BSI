8afb67ef009d95ad12d897513bd05843885fc9b7
Mariana <sousa06390@gmail.com>
2026-09-17 11:51:20 -0300

    Add files via upload

---
+++ Desenvolvimento Pag Web II/Revisão Prova I/javascript.js
@@ -0,0 +1,37 @@
+/*1.Crie um botão centralizado (na horizontal e vertical) que quando clicado 
+muda a cor de fundo da página na seguinte sequência : azul -> verde -> amarelo -> vermelho ->
+rosa -> azul ...
+
+2.Crie um botão e um parágrafo de texto. A cada clique no botão, se o texto estiver visível, ele é
+ocultado, e se estiver oculto, torna-se visível.
+
+3.Crie dois inputs de texto e um botão. O usuário digita um texto no primeiro input e um número
+no segundo. Ao clicar no botão exibido no console o texto do primeiro input N vezes, onde N é o
+número digitado no segundo input.
+
+4.Adicione o seguinte object ao seu código JavaScript :
+
+const box = { 
+    "largura":"150px"
+    "altura":"200px"
+    "cores":{
+        "fundo":"blue"
+        "borda":"red"
+        }
+};
+
+E no HTML :
+<div id="html_box"></div>
+
+Adicione um botão que quando o usuário clicar, aplique os estilos definidos na variável box na div do HTML.
+*/ 
+
+const cores = ["blue", "green", "yellow", "red", "pink"];
+const box = { 
+    "largura":"150px",
+    "altura":"200px",
+    "cores":{
+        "fundo":"blue",
+        "borda":"red"
+        }
+};
