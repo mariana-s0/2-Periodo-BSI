0e6c7bec0df3b2b63e0973116e682540aff98ddf
Mariana <sousa06390@gmail.com>
2026-09-21 16:38:06 -0300

    Delete Estrutura de Dados/Lista 01/questao17.c

--- Estrutura de Dados/Lista 01/questao17.c
+++
@@ -1,7 +0,0 @@
-/*Questão 17
-Escreva um programa que leia um número inteiro n, correspondente ao tamanho de um vetor. Em
-seguida, aloque dinamicamente um vetor de n posições e preencha-o com números inteiros fornecidos
-pelo usuário. O programa deve então remover todas as ocorrências de valores repetidos, de modo que
-cada número apareça apenas uma vez. Para isso, reorganize o vetor e use a função realloc para
-reduzir seu tamanho ao mínimo necessário. Por fim, exiba o vetor resultante, contendo apenas os
-elementos únicos.*/
