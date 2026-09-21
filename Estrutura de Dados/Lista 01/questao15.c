00b949e8111bf2cf6f670b4288707b3a2ff7b1ea
Mariana <sousa06390@gmail.com>
2026-09-21 16:33:21 -0300

    Delete Estrutura de Dados/Lista 01/questao15.c

--- Estrutura de Dados/Lista 01/questao15.c
+++
@@ -1,5 +0,0 @@
-/*Questão 15
-Escreva um programa que aloque dinamicamente um vetor de n inteiros. Após o preenchimento do
-vetor, permita que o usuário escolha uma posição para remover um elemento.
-Após a remoção, reorganize os elementos para que não fiquem “buracos” no vetor, e utilize realloc
-para ajustar o tamanho da área de memória.Exiba o vetor resultante e, ao final, libere a memória.*/
