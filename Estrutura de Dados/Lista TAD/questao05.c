18976c9b253bdf3f77d765b930bdd160487e6db8
Mariana <sousa06390@gmail.com>
2026-09-23 09:30:26 -0300

    Delete Estrutura de Dados/Lista TAD/questao05.c

--- Estrutura de Dados/Lista TAD/questao05.c
+++
@@ -1,9 +0,0 @@
-/*Questão 05
-Implemente um TAD Número Complexo utilizando ponteiros, no qual cada número possui os
-campos real e imaginário. O TAD deve incluir uma função de criação para alocar dinamicamente
-um número complexo e inicializar seus valores, e uma função de destruição para liberar a memória
-alocada. Além disso, implemente funções setters para alterar os valores dos campos real e
-imaginário e funções getters para acessá-los. O TAD deve também conter funções para realizar as
-operações de soma, subtração, multiplicação e divisão entre dois números complexos, retornando o
-resultado como um novo número complexo. Por fim, escreva um programa que utilize e teste todas
-as operações do TAD Número Complexo.*/
