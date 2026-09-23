9bf522bc0a16bf6b0941ca58fa68d655577be7b0
Mariana <sousa06390@gmail.com>
2026-09-23 09:31:14 -0300

    Delete Estrutura de Dados/Lista TAD/questao09.c

--- Estrutura de Dados/Lista TAD/questao09.c
+++
@@ -1,5 +0,0 @@
-/*Questão 09
-Implemente um TAD para representar cadeias de caracteres de tamanho variável. O TAD deve
-conter uma função de criação a partir de uma string tradicional (literal), destruição, funções para
-retornar o tamanho da string, concatenar duas strings (retornando uma nova), converter todas as
-letras para maiúsculas e buscar a ocorrência de um caractere.*/
